a3c75eefc1c5aeaf140e256b1bf0b33dd2ab7810
Proloy <amamun.nstu@gmail.com>
2022-03-01 22:12:21 +0600

    updated about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="about blue darken-3 white--text text-center pb-7">
+      <h4>Department of Information and Communication Engineering </h4>
+    </div>
+    <v-layout row wrap class="mt-10">
+
+      <v-flex xs12 md4 class="">
+        <v-img src="@/assets/chairman.jpg" max-height="150" max-width="120"></v-img>
+      </v-flex>
+
+      <v-flex xs12 md8 class="ml-8 mt-10">
+        Welcome to Information and Communication Engineering Department (ICE) and thank you for visiting our webpage. It
+        is with great pleasure that we present to you the key as well as detailed information on our department. ICE
+        department has been begun not long in Noakhali Science and Technology University. Yet, our aim is to be a world
+        class education centre.
+
+      </v-flex>
+    </v-layout>
   </div>
 </template>
