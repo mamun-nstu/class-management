b0ba117bcd7a5c446943cb150d0b0133caaa8699
Proloy <amamun.nstu@gmail.com>
2022-03-13 22:36:48 +0600

    coverts style tag to css classes

--- client/src/components/StudentCard.vue
+++ client/src/components/StudentCard.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <div id="student-card" :style="{width: '300px', paddingLeft: '10px', paddingRight: '10px', fontSize: '14px', margin: 'auto'}">
-      <div :style="{margin: 'auto'}">
-        <div :style="{margin: 'auto', display: 'flex', flexDirection: 'column'}">
-          <img :style="{margin: 'auto'}" :src="nstu_logo" height="80" width="60"/>
-          <div :style="{display: 'flex', justifyContent: 'center'}">
-            <h2 :style="{fontSize: '12px'}">Noakhali Science and Technology University</h2>
-          </div>
-          
+    <div id="student-card" class="student-card">
+      <div>
+        <div class="nstu-logo">
+          <img :src="nstu_logo"/>
+        </div>
+        <div>
+          <h2>Noakhali Science and Technology University</h2>
         </div>
       </div>
-      <div :style="{display: 'flex', flexDirection: 'column', justifyContent: 'center'}">
-        <p :style="{fontSize: '12px', margin: 0}">Name: {{ student.full_name }}</p>
-        <p :style="{fontSize: '12px', margin: 0}">Student ID: {{ student.student_id }}</p>
-        <p :style="{fontSize: '12px', margin: 0}">Batch: {{ student.batch ? student.batch.name : '' }}</p>
+      <div>
+        <p>Name: {{ student.full_name }}</p>
+        <p>Student ID: {{ student.student_id }}</p>
+        <p>Batch: {{ student.batch ? student.batch.name : '' }}</p>
       </div>
-      <div :style="{padding: '10px'}">
-        <img :src="this.qr_code_base64" height="100px" width="100px"/>
+      <div class="qr-img-container">
+        <img :src="this.qr_code_base64" class="qr-img"/>
       </div>
     
     </div>
@@ -46,6 +45,42 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.student-card {
+  width: 300px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 1px;
+  margin: auto;
+  
+  .nstu-logo {
+    display: flex;
+    justify-content: center;
+    
+    img {
+      height: 80px;
+      width: 60px;
+    }
+  }
+  
+  
+  .qr-img-container {
+    padding-top: 10px;
+    
+    img.qr-img {
+      height: 100px;
+      width: 100px;
+    }
+  }
+  
+  h2 {
+    font-size: 13px;
+  }
+  
+  p {
+    font-size: 12px;
+    margin: 0;
+    padding: 0;
+  }
+}
 </style>
